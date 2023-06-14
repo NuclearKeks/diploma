19f687394ae70b6a25eab15c8eaca129fdf3aff6
NuclearKeks <dostojnov@mail.ru>
2023-06-14 15:52:46 +0300

    Final result

--- data_restoring/linear_approx.py
+++ data_restoring/linear_approx.py
@@ -1,5 +1,3 @@
-# import random
-# from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -75,49 +73,3 @@ def linapprox_test(data: pd.DataFrame):
                 ultranew_data[row][col] = (column[2] - column[0])/2 + column[0]
     ultranew_data = pd.DataFrame(ultranew_data, columns=data_df.columns)
     return [ultranew_data, data_df]
-
-
-if __name__ == '__main__':
-    from matplotlib import pyplot as plt
-    data = pd.read_excel('nan_200.xlsx')
-    data_orig = pd.read_excel('kalmanK1.xlsx')
-    data = data[data.columns[1:]]
-    COL = data.columns[0]
-    MARKER_SIZE = 5
-
-    # real example
-    # new_data = linapprox(data)
-    # data_after = linapprox(data)
-    # new_data = data_after[0]
-    new_data = linapprox(data)
-    plt.scatter(new_data.index, new_data[COL],
-                s=MARKER_SIZE, c='#2ec720', label='restored')
-    plt.scatter(data.index, data[COL], s=MARKER_SIZE,
-                c='#2118d9', label='original')
-    plt.legend()
-    plt.show()
-
-    # # preparation for combination
-    # prep_data = linapprox_test(data)
-    # prep_data[0].to_excel('prep_linapprox.xlsx')
-    # plt.plot(prep_data[1].index, prep_data[1]
-    #          [COL], c='#2118d9', label='original')
-    # plt.plot(prep_data[0].index, prep_data[0]
-    #          [COL], c='#2ec720', label='prepared')
-    # plt.legend()
-    # plt.show()
-
-    # #errors
-    # errors = []
-    # for x in range(len(new_data.columns)):
-    #     nans = data_after[1][x]
-    #     nans_indexes = data_after[2][x]
-    #     errors1 = []
-    #     for i, v in enumerate(nans):
-    #         orig = data_orig[data.columns[x]][nans_indexes[i]]
-    #         # print(orig, v)
-    #         diapason = np.max(data_orig[data.columns[x]]) - \
-    #             np.min(data_orig[data.columns[x]])
-    #         errors1.append(abs(v-orig)/diapason)
-    #     errors.append(np.mean(errors1))
-    # print(f'Error_cool:{np.mean(errors)}')
--- data_restoring/vinsor_right.py
+++ data_restoring/vinsor_right.py
@@ -1,10 +1,3 @@
-# def vins_right(data1:list):
-#     data = data1.copy()
-#     for i in range(len(data)-1, -1, -1):
-#         if data[i] is None and i <len(data)-1:
-#             data[i] = data[i+1]
-#     return data
-
 import pandas as pd
 import numpy as np
 
@@ -40,49 +33,3 @@ def vins_right_test(data: pd.DataFrame):
             ultranew_data[row][col] = column[row+1]
     ultranew_data = pd.DataFrame(ultranew_data, columns=data_df.columns)
     return [ultranew_data, data_df]
-
-
-if __name__ == '__main__':
-    from matplotlib import pyplot as plt
-    data = pd.read_excel('nan_200.xlsx')
-    data_orig = pd.read_excel('kalmanK1.xlsx')
-    print(data.head())
-    data = data[data.columns[1:]]
-    COL = data.columns[10]
-    MARKER_SIZE = 5
-
-    # real example
-    # data_after = vins_right(data)
-    # new_data = data_after[0]
-    new_data = vins_right(data)
-    plt.scatter(new_data.index, new_data[COL],
-                s=MARKER_SIZE, c='#2ec720', label='restored')
-    plt.scatter(data.index, data[COL], s=MARKER_SIZE,
-                c='#2118d9', label='original')
-    plt.legend()
-    plt.show()
-
-    # #errors
-    # errors = []
-    # for x in range(len(new_data.columns)):
-    #     nans = data_after[1][x]
-    #     nans_indexes = data_after[2][x]
-    #     errors1 = []
-    #     for i, v in enumerate(nans):
-    #         orig = data_orig[data.columns[x]][nans_indexes[i]]
-    #         # print(orig, v)
-    #         diapason = np.max(data_orig[data.columns[x]]) - \
-    #             np.min(data_orig[data.columns[x]])
-    #         errors1.append(abs(v-orig)/diapason)
-    #     errors.append(np.mean(errors1))
-    # print(f'Error_cool:{np.mean(errors)}')
-
-    # # preparation for combinaton
-    # prep_data = vins_right_test(data)
-    # prep_data[0].to_excel('prep_vinsright.xlsx')
-    # plt.scatter(prep_data[0].index, prep_data[0][COL],
-    #             s=MARKER_SIZE, c='#2ec720', label='prepared')
-    # plt.scatter(prep_data[1].index, prep_data[1][COL],
-    #             s=MARKER_SIZE, c='#2118d9', label='original')
-    # plt.legend()
-    # plt.show()
